94c4e5d1206d679a611cba9f78d84867efa6a6da
Christopher Acornley <c.acornley@abertay.ac.uk>
2025-04-22 23:16:07 +0100

    Base class for dataset loader

---
+++ datasets/dataset_loader.py
@@ -0,0 +1,8 @@
+class DatasetLoader:
+    def __init__(self, options):
+        self.train_dataset_src = options.train_data_src
+        self.test_dataset_src = options.test_data_src
+
+    def load(self):
+        print("Do nothing")
+        pass
