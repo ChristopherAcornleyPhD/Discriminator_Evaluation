84605c675a908f02b610f8cfaf0f93122d917735
Christopher Acornley <c.acornley@abertay.ac.uk>
2025-04-28 00:39:11 +0100

    Chnaged name of file

--- utils/metric_writer.py
+++ utils/metric_writer.py
@@ -1,9 +1,10 @@
 import os
 import pandas as pd
 
-class Metrics():
+class MetricWriter():
     def __init__(self, opt):
         self.opt = opt
+        self.data_to_save = dict()
         if self.opt.save_metrics:
             try:
                 self.workbook_path = os.path.join(self.opt.experiment_folder, self.opt.metrics_workbook_name)
@@ -16,4 +17,6 @@ class Metrics():
 
     def close(self):
         if self.writer is not None:
+            for key, value in self.data_to_save.items():
+                value.to_excel(self.writer, sheet_name=key)
             self.writer.close()
