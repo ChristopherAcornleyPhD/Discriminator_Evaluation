f27797260a1925d24259c0be707fddee20031ca4
Christopher Acornley <c.acornley@abertay.ac.uk>
2025-05-05 23:31:43 +0100

    Moved setup of models and instrutors to their own managers. Extended argument list.

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import argparse
-from instructors.RICO_instructor import RICOInstructor
-from datasets.RICO_loader import RICOLoader
-from utils.RICO_metric_writer import RICOMetricWriter
 
-from models.BiLSTM_models import EmptyBiLSTM, LinearBiLSTM, EmbeddingBiLSTM
-from models.LSTM_models import EmptyLSTM, LinearLSTM, EmbeddingLSTM
-from models.GRU_models import EmptyGRU, LinearGRU, EmbeddingGRU
-from models.Vanilla_models import EmbeddingVanilla, LinearVanilla, EmptyVanilla
-from models.DNN_models import EmptyDNN, LinearDNN, EmbeddingDNN
-from models.CNN_models import EmptyCNN, LinearCNN, EmbeddingCNN
-from models.Pooling_models import EmptyPooling, LinearPooling, EmbeddingPooling
+from managers.RICO_manager import RICOManager
+from managers.IMDB_manager import IMDBManager
 
 from utils.utils import create_experiment_folder
 
 def setup_config(parser):
-    parser.add_argument("--train_src", default="..\\RicoDatasetLoader\\small_sample\\train", type=str)
-    parser.add_argument("--test_src", default="..\\RicoDatasetLoader\\small_sample\\test", type=str)
+    parser.add_argument("--epochs", default=5, type=int)
     parser.add_argument("--experiment_folder", default="experiment0", type=str)
     parser.add_argument("--output_folder", default="experiments", type=str)
-    parser.add_argument("--model_folder", default="models", type=str)
     parser.add_argument("--time_epochs", default=True, type=bool)
     parser.add_argument("--time_loading", default=True, type=bool)
     parser.add_argument("--save_models", default=True, type=bool)
     parser.add_argument("--save_metrics", default=True, type=bool)
-    parser.add_argument("--metrics_workbook_name", default="metrics_workbook.xlsx", type=str)
-    parser.add_argument("--epochs", default=5, type=int)
-    parser.add_argument("--training", default=True, type=bool)
-    parser.add_argument("--testing", default=True, type=bool)
-    parser.add_argument("--model_load_path", default="", type=str)
+
+    parser.add_argument("--RICO_training", default=True, type=bool)
+    parser.add_argument("--RICO_testing", default=True, type=bool)
+    parser.add_argument("--RICO_model_folder", default="RICO_models", type=str)
+    parser.add_argument("--RICO_vocab_size", default=101, type=int)
+    parser.add_argument("--RICO_train_src", default="..\\RicoDatasetLoader\\small_sample\\train", type=str)
+    parser.add_argument("--RICO_test_src", default="..\\RicoDatasetLoader\\small_sample\\test", type=str)
+    parser.add_argument("--RICO_metrics_workbook_name", default="rico_metrics_workbook.xlsx", type=str)
+
+    parser.add_argument("--IMDB_training", default=True, type=bool)
+    parser.add_argument("--IMDB_testing", default=True, type=bool)
+    parser.add_argument("--IMDB_model_folder", default="IMDB_models", type=str)
+    parser.add_argument("--IMDB_buffer_size", default=10000, type=int)
+    parser.add_argument("--IMDB_batch_size", default=64, type=int)
+    parser.add_argument("--IMDB_vocab_size", default=1000, type=int)
+    parser.add_argument("--IMDB_metrics_workbook_name", default="imdb_metrics_workbook.xlsx", type=str)
     return parser
 
 if __name__ == '__main__':
@@ -37,46 +38,10 @@ if __name__ == '__main__':
 
     create_experiment_folder(opt)
 
-    RICO_writer = RICOMetricWriter(opt)
+    if opt.RICO_training or opt.RICO_testing:
+        rico_manager = RICOManager(opt)
+        rico_manager.run()
 
-    data_loader = RICOLoader(opt)
-    dataset = data_loader.load()
-
-    model_list = []
-
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmbeddingDNN(), name="dnn_embedding"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=LinearDNN(), name="dnn_linear"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmptyDNN(), name="dnn_empty"))
-
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmbeddingBiLSTM(), name="bilstm_embedding"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=LinearBiLSTM(), name="bilstm_linear"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmptyBiLSTM(), name="bilstm_empty"))
-
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmbeddingCNN(), name="cnn_embedding"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=LinearCNN(), name="cnn_linear"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmptyCNN(), name="cnn_empty"))
-
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmbeddingLSTM(), name="lstm_embedding"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=LinearLSTM(), name="lstm_linear"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmptyLSTM(), name="lstm_empty"))
-
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmbeddingGRU(), name="gru_embedding"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=LinearGRU(), name="gru_linear"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmptyGRU(), name="gru_empty"))
-
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmbeddingVanilla(), name="vanilla_embedding"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=LinearVanilla(), name="vanilla_linear"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmptyVanilla(), name="vanilla_empty"))
-
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmbeddingPooling(), name="gp_embedding"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=LinearPooling(), name="gp_linear"))
-    model_list.append(RICOInstructor(opt, dataset, classifier=EmptyPooling(), name="gp_empty"))
-
-    for instructor in model_list:
-        try:
-            instructor.run(RICO_writer)
-        except Exception as e:
-            print(e)
-            print("Cannot run " + instructor.name)
-
-    RICO_writer.close()
+    if opt.IMDB_training or opt.IMDB_testing:
+        imdb_manager = IMDBManager(opt)
+        imdb_manager.run()
